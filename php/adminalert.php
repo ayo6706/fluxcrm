e434fee7295543a2e096a1180be04a01b03e34ae
ayomide onibokun <35877863+ayo6706@users.noreply.github.com>
2020-08-15 02:25:52 +0100

    Update adminalert.php

--- php/adminalert.php
+++ php/adminalert.php
@@ -31,7 +31,7 @@ $currentDate = date('Y-m-d'); //this will get the current date ie when this was 
         
         $host = "ssl://mail.repotecc.com";
         $username = "ayomide@repotecc.com";
-        $password = "Vadtontech1953.";
+        $password = "";
         $port = "465";
         $to = "ayo6706@gmail.com"; //gets the user email address
     
